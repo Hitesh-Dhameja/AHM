5a1ac73320679982148c84556a8e614de9c9fccf
Hitesh-Dhameja <65720538+Hitesh-Dhameja@users.noreply.github.com>
2020-12-16 20:40:42 +0530

    Update _dbconnect.php

--- AHM/common/_dbconnect.php
+++ AHM/common/_dbconnect.php
@@ -1,10 +1,10 @@
 <?php
     //connecting to db
     //$servername = "localhost:3307";
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "ahm";
+    $servername = " sql12.freemysqlhosting.net";
+    $username = "sql12382461";
+    $password = "nHadcdCfzd";
+    $database = "sql12382461";
     $conn = mysqli_connect($servername, $username, $password, $database);
     if(!$conn){
         die("Sorry! Cannot connect to database right now");
